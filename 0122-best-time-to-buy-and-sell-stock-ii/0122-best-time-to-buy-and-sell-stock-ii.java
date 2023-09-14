de95785968eb0c3466e62439ff38166ed50f49a5
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2023-09-15 01:09:20 +0530

    Time: 1 ms (85.33%), Space: 43.9 MB (95.74%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -3,9 +3,12 @@ class Solution {
         int currMinimal = prices[0];
         int profit = 0;
         for(int i=1;i<prices.length;i++){
+            //selling wrt current minimal if current price is higher than current minimal
             if(prices[i]>currMinimal){
                 profit +=prices[i]-currMinimal;
             }
+            //if price<minimal it is updated. if price>minimal,it is sold wrt to current minimal and then minimal is updated
+            //as minimal moves only further along day and updates on each sale it gives the illusion of having only one stock at a time
             currMinimal=prices[i];
         }
         return profit;
