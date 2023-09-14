c27bfeea8540517d8893590fd006d0edc2a3d711
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2023-09-14 23:59:59 +0530

    Time: 9 ms (58.54%), Space: 39.8 MB (76.54%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -8,21 +8,53 @@ class Solution {
         return true;
     }
     
-    public boolean solveSudoku(char[][] board) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(board[i][j]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isValid(board,i,j,c)){
-                            board[i][j]=c;
-                            if(solveSudoku(board)) return true;
-                            else board[i][j]='.';
-                        }
-                    }
-                    return false;
+    // public boolean solveSudoku(char[][] board) {
+    //     for(int i=0;i<board.length;i++){
+    //         for(int j=0;j<board[0].length;j++){
+    //             if(board[i][j]=='.'){
+    //                 for(char c='1';c<='9';c++){
+    //                     if(isValid(board,i,j,c)){
+    //                         board[i][j]=c;
+    //                         if(solveSudoku(board)) return true;
+    //                         else board[i][j]='.';
+    //                     }
+    //                 }
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    
+    private boolean solve(char[][] board, int row, int col){
+        if(row==board.length) return true;
+        
+        int nrow=0,ncol=0;
+        
+        if(col!=board[0].length-1){
+            nrow=row;
+            ncol=col+1;
+        }else{
+            nrow=row+1;
+            ncol=0;
+        }
+        
+        if(board[row][col]!='.'){
+            if(solve(board,nrow,ncol)) return true;
+        }else{
+            for(char c='1';c<='9';c++){
+                if(isValid(board,row,col,c)){
+                    board[row][col]=c;
+                    if(solve(board,nrow,ncol)) return true;
+                    else board[row][col]='.';
                 }
             }
         }
-        return true;
+        
+        return false;
+    }
+    
+    public void solveSudoku(char[][] board){
+        solve(board,0,0);
     }
 }
