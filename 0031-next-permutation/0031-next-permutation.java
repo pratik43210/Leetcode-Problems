b79ccf60196c408676c2f18c21f4eda617c7171c
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-13 16:08:11 +0530

    Time: 1 ms (18.58%), Space: 43 MB (60.57%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,12 +1,12 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        List<Integer> A=new ArrayList<Integer>();
-        for(int a:nums){
-            A.add(a);
-        }
-        int idx=-1;
+        List<Integer> A=new ArrayList<>();
         int n=nums.length;
+        for(int i: nums){
+            A.add(i);
+        }
         
+        int idx=-1;
         for(int i=n-2;i>=0;i--){
             if(A.get(i)<A.get(i+1)){
                 idx=i;
@@ -15,16 +15,15 @@ class Solution {
         }
         
         if(idx==-1){
-            Collections.reverse(A);
-            for(int i=0;i<A.size();i++){
-                nums[i]=A.get(i);
+            for(int i=0;i<n;i++){
+                nums[i]=A.get(n-i-1);
             }
             return;
         }
         
         for(int i=n-1;i>idx;i--){
             if(A.get(i)>A.get(idx)){
-                Collections.swap(A,i,idx);
+                Collections.swap(A,idx,i);
                 break;
             }
         }
@@ -32,8 +31,9 @@ class Solution {
         List<Integer> sublist=A.subList(idx+1,n);
         Collections.reverse(sublist);
         
-        for(int i=0;i<A.size();i++){
+        for(int i=0;i<n;i++){
             nums[i]=A.get(i);
         }
+        return;
     }
 }
