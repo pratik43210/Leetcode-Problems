95f241c64aec3f1680b2041d160892335d4224b7
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-14 17:57:42 +0530

    Time: 10 ms (26.15%), Space: 46.6 MB (42.51%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -11,23 +11,30 @@ class Solution {
         List<int[]> ans=new ArrayList<>();
         
         for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
+//             int start=intervals[i][0];
+//             int end=intervals[i][1];
             
-            if(!ans.isEmpty() && end<=ans.get(ans.size()-1)[1]){
-                continue;
-            }
+//             if(!ans.isEmpty() && end<=ans.get(ans.size()-1)[1]){
+//                 continue;
+//             }
             
-            for(int j=i+1;j<n;j++){
-                if(intervals[j][0]<=end){
-                    end=Math.max(end,intervals[j][1]);
-                }else{
-                    break;
-                }
+//             for(int j=i+1;j<n;j++){
+//                 if(intervals[j][0]<=end){
+//                     end=Math.max(end,intervals[j][1]);
+//                 }else{
+//                     break;
+//                 }
+//             }
+//             int[] finalnterval={start,end};
+//             ans.add(finalnterval);
+            
+            if(ans.isEmpty()||intervals[i][0]>ans.get(ans.size()-1)[1]){
+                ans.add(intervals[i]);
+            }else{
+                ans.get(ans.size()-1)[1]=Math.max(intervals[i][1],ans.get(ans.size()-1)[1]);
             }
-            int[] finalnterval={start,end};
-            ans.add(finalnterval);
         }
+        
         int s=ans.size();
         int[][] ansArr=new int[s][2];
         for(int i=0;i<s;i++){
