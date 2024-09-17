689afb50153c6c3d54302312117cc1f91745e527
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-17 17:56:37 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (95.19%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,39 +1,47 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        List<Integer> A=new ArrayList<>();
-        int n=nums.length;
-        for(int i: nums){
-            A.add(i);
-        }
+        int n = nums.length;
+        int idx = -1;
         
-        int idx=-1;
-        for(int i=n-2;i>=0;i--){
-            if(A.get(i)<A.get(i+1)){
-                idx=i;
+        // Find the first decreasing element from the end
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
                 break;
             }
         }
         
-        if(idx==-1){
-            for(int i=0;i<n;i++){
-                nums[i]=A.get(n-i-1);
-            }
+        // If no such element is found, reverse the array
+        if (idx == -1) {
+            reverse(nums, 0, n - 1);
             return;
         }
         
-        for(int i=n-1;i>idx;i--){
-            if(A.get(i)>A.get(idx)){
-                Collections.swap(A,idx,i);
+        // Find the smallest element larger than nums[idx]
+        for (int i = n - 1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums, idx, i);
                 break;
             }
         }
         
-        List<Integer> sublist=A.subList(idx+1,n);
-        Collections.reverse(sublist);
-        
-        for(int i=0;i<n;i++){
-            nums[i]=A.get(i);
+        // Reverse the subarray after idx
+        reverse(nums, idx + 1, n - 1);
+    }
+    
+    // Helper function to swap two elements in an array
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    // Helper function to reverse a subarray
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
         }
-        return;
     }
 }
