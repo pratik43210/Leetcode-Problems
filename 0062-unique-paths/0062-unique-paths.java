91bd3aa14da79405b4c81aaf69f7bf0ce87df8a3
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-20 00:22:26 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (77.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //we start at 0,0 so, possible steps in right = m-1
-        //possible steps in downward direction = n-1
-        //total number of possible steps
         int N = m+n-2;
-        // we can either find total combination by finding
-        //nCr for the n-1 right paths or m-1 down paths
         int r= m-1;
         
         double res=1;
