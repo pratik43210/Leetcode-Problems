605e50bf3f81060977963713c89f10e1dc74e2d2
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-23 17:52:00 +0530

    Time: 644 ms (5.04%), Space: 45.4 MB (15.95%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,45 +1,29 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length; // size of the array
-        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
 
-        // sort the given array:
-        Arrays.sort(nums);
+        
 
-        // calculating the quadruplets:
         for (int i = 0; i < n; i++) {
-            // avoid the duplicates while moving i:
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < n; j++) {
-                // avoid the duplicates while moving j:
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                // 2 pointers:
-                int k = j + 1;
-                int l = n - 1;
-                while (k < l) {
-                    long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if (sum == target) {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[k]);
-                        temp.add(nums[l]);
-                        ans.add(temp);
-                        k++;
-                        l--;
-
-                        // skip the duplicates:
-                        while (k < l && nums[k] == nums[k - 1]) k++;
-                        while (k < l && nums[l] == nums[l + 1]) l--;
-                    } else if (sum < target) k++;
-                    else l--;
+                Set<Long> hashSet=new HashSet<>();
+                for(int k=j+1;k<n;k++){
+                    long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    long fourth=target-sum;
+                    if(hashSet.contains(fourth)){
+                        List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k],(int) fourth);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                    hashSet.add((long)nums[k]);
                 }
             }
         }
+        
+        List<List<Integer>> ans=new ArrayList<>(set); 
 
         return ans;
     }
