39b1861c8dc831035759d696a015d98bf87b718d
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-23 18:13:14 +0530

    Time: 15 ms (88.61%), Space: 43.9 MB (50.63%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,39 +1,43 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n = nums.length; // size of the array
         List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
 
+        // Sort the array first
         Arrays.sort(nums);
 
         for (int i = 0; i < n; i++) {
-            if(i>0 && nums[i]==nums[i-1]) continue;
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             for (int j = i + 1; j < n; j++) {
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int left=j+1;
-                int right=n-1;
-                //take the two pointers left and right
-                while(left<right){
-                    long sum=nums[i];
-                    sum+= nums[j];
-                    sum+= nums[left];
-                    sum+= nums[right];
-                    if(sum<target){
+                // Skip duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1, right = n - 1;
+                // Use two-pointer technique for the remaining two numbers
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum < target) {
                         left++;
-                    }else if(sum>target){
+                    } else if (sum > target) {
                         right--;
-                    }else{
-                        List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[left],nums[right]);
-                        ans.add(temp);
+                    } else {
+                        // Found a valid quadruplet
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        // Move left and right to the next different numbers to avoid duplicates
                         left++;
                         right--;
-                        while(left<right && nums[left]==nums[left-1]) left++;
-                        while(left<right && nums[right]==nums[right+1]) right--;
+
+                        // Skip duplicates for left and right pointers
+                        while (left < right && nums[left] == nums[left - 1]) left++;
+                        while (left < right && nums[right] == nums[right + 1]) right--;
                     }
                 }
             }
         }
-        
-
         return ans;
     }
 }
