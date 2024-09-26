a1850f5acf2e70b3cdc6983744005d8c3d1b85fb
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-09-27 01:14:25 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (83.14%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,35 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode temp = head;  
-   
-   // Initialize pointer 'prev' to NULL,
-   // representing the previous node
-   ListNode prev = null;  
-   
-   // Traverse the list, continue till
-   // 'temp' reaches the end (NULL)
-   while(temp != null){  
-       // Store the next node in
-       // 'front' to preserve the reference
-       ListNode front = temp.next;  
-       
-       // Reverse the direction of the
-       // current node's 'next' pointer
-       // to point to 'prev'
-       temp.next = prev;  
-       
-        // Move 'prev' to the current
-        // node for the next iteration
-       prev = temp;  
-       
-        // Move 'temp' to the 'front' node
-        // advancing the traversal
-       temp = front; 
-   }
-   
-   // Return the new head of
-   // the reversed linked list
-   return prev;  
+        ListNode curr=head;
+        ListNode prev=null;
+        
+        while(curr!=null){
+            ListNode front=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=front;
+        }
+        
+        return prev;
     }
 }
