0d4e3835faa7be617ed01169febb32a5ef2bf098
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-02 20:06:06 +0530

    Time: 6 ms (54.69%), Space: 68.8 MB (25.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     static ListNode reverseList(ListNode list){
+        //reverses the list starting from the node in argument
         ListNode curr=list;
         ListNode prev=null;
         while(curr!=null){
@@ -23,6 +24,9 @@ class Solution {
     static ListNode findMiddle(ListNode head){
         ListNode fast=head;
         ListNode slow=head;
+        //slow will end up in middle and if there are two middles,
+        //it will end up at second middle
+        //for palindrome input, we would have two middles
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
@@ -31,11 +35,15 @@ class Solution {
     }
     public boolean isPalindrome(ListNode head) {
         if(head==null || head.next==null) return true;
-        
+        //first node
         ListNode start=head;
+        //middle node (e.g. for 1,2,2,1-> second 2)
         ListNode mid=findMiddle(head);
+        //reverse the list from middle to end (e.g. 1,2,1,2)
+        //store the returned start node of this part in mid
         mid=reverseList(mid);
         
+        //compare the two parts of linked list
         while(mid!=null){
             if(start.val!=mid.val) return false;
             start=start.next;
