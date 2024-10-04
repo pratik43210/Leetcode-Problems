3840aaab46e8f8e9cfd7ed489e66f96edcc95506
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-04 15:23:30 +0530

    Time: 0 ms (100.00%), Space: 43.9 MB (98.02%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,17 +15,20 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+        
         Node temp=head;
         
+        //link copy node in between the node
         while(temp!=null){
-            Node copyNode=new Node(temp.val);
-            copyNode.next=temp.next;
-            temp.next=copyNode;
+            Node newNode=new Node(temp.val);
+            newNode.next=temp.next;
+            temp.next=newNode;
             temp=temp.next.next;
         }
         
         temp=head;
         
+        //link random to the copy nodes
         while(temp!=null){
             Node copyNode=temp.next;
             if(temp.random!=null) copyNode.random=temp.random.next;
@@ -33,13 +36,18 @@ class Solution {
             temp=temp.next.next;
         }
         
+        //make a dummy node
         Node dNode=new Node(-1);
+        //make a node to iterate from dummy node
         Node res=dNode;
         temp=head;
         
         while(temp!=null){
+            //point res.next to copy node
             res.next=temp.next;
+            //fix the original linked list pointing it to next of next
             temp.next=temp.next.next;
+            //move temp and res to next node
             temp=temp.next;
             res=res.next;
         }
