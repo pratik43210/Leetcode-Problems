02c4bbe2b68016c04a86279ff7aa74f7c4019f20
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-05 21:44:31 +0530

    Time: 11 ms (31.77%), Space: 48.6 MB (16.85%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int gi=0,si=0;
+        int count=0;
+        
+        while(gi<g.length && si<s.length){
+            if(g[gi]<=s[si]){
+                count++;
+                gi++;
+            }
+            si++;
+        }
+        
+        return count;
+    }
+}
