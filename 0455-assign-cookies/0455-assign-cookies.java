735d830c961407337d8f5034146ffdd4bd82ce44
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-05 21:48:00 +0530

    Time: 11 ms (31.77%), Space: 48.6 MB (23.69%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,19 +1,20 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
+        //sort the greed and size arrays
         Arrays.sort(g);
         Arrays.sort(s);
         
+        //create a greed index and size index, initiate with 0
         int gi=0,si=0;
-        int count=0;
         
+        //
         while(gi<g.length && si<s.length){
             if(g[gi]<=s[si]){
-                count++;
                 gi++;
             }
             si++;
         }
         
-        return count;
+        return gi;
     }
 }
