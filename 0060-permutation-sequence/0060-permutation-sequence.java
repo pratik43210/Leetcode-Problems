e9a79015cd32c468d3741dde5f27e56687fb2ccc
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-12 00:04:42 +0530

    Time: 2 ms (54.56%), Space: 41.2 MB (43.92%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,30 +1,5 @@
 class Solution {
-//     private void swap(char s[], int i, int j){
-//         char c=s[i];
-//         s[i]=s[j];
-//         s[j]=c;
-//     }
-//     private void helper(char s[], int idx, ArrayList<String> res){
-//         if(idx==s.length){
-//             String str=new String(s);
-//             res.add(str);
-//         }
-        
-//         for(int i=idx;i<s.length;i++){
-//             swap(s,i,idx);
-//             helper(s, idx+1, res);
-//             swap(s,i,idx);
-//         }
-//     }
     public String getPermutation(int n, int k) {
-        // String s="";
-        // ArrayList<String> res=new ArrayList<>();
-        // for(int i=1;i<=n;i++){
-        //     s+=i;
-        // }
-        // helper(s.toCharArray(),0,res);
-        // Collections.sort(res);
-        // return res.get(k-1);
         int fact=1;
         ArrayList<Integer> nums=new ArrayList<>();
         for(int i=1; i<n;i++){
