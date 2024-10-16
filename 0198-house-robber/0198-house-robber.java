9346c2dcb3dc2672e1f5e241e827b7bdaf59445a
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-16 22:32:02 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (97.70%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int robHouse(int[] nums, int n , int[] dp){
+        //base condition
+        if(n==0) return nums[n];
+        if(n<0) return 0;
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        //we either pick current value or not
+        //if we pick current we cannot pic adjacent
+        //we go to current-2 index
+        int pick=nums[n] + robHouse(nums,n-2,dp);
+        
+        int notPick=0 + robHouse(nums,n-1,dp);
+        
+        return dp[n]=Math.max(pick,notPick);
+    }
+    public int rob(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        
+        for(int i=0;i<n;i++){
+            dp[i]=-1;
+        }
+        
+        return robHouse(nums,n-1,dp);
+    }
+}
