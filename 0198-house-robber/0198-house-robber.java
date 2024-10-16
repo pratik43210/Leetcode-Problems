6ba76bf5a735fee26398f2a19fa4b171a2f23ce2
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-16 23:32:16 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (36.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,13 +4,22 @@ class Solution {
         int[] dp= new int[n];
         dp[0]=nums[0];
         
+        //we already have dp[0] and leave will account for it
+        //when calculating dp[1] so we start loopinf from 1
         for(int i=1;i<n;i++){
+            //nums[i] will be the amount robbed in current house
             int take=nums[i];
             
+            //dp[i-2] is the maximun amount robbed till i-2
+            //since we rob current house, we leave i-1(adjacent)
+            //so we take max amount robbed till i-2
             if(i>1) take+=dp[i-2];
             
+            //dp[i-1] is maximum amount robbed till i-1
+            //we left current so we consider max till i-1
             int leave=dp[i-1];
             
+            //max amount robbed till current house
             dp[i]=Math.max(take,leave);
         }
         
