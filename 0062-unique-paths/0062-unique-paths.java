d92dd5b079d2f43d96f6d511c87a1cc78af8ebec
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-10-21 23:18:33 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (49.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,21 @@
 class Solution {
+    static int f(int i, int j, int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0||j<0) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int left=f(i,j-1,dp);
+        int up=f(i-1,j,dp);
+        
+        return dp[i][j]=up+left;
+    }
     public int uniquePaths(int m, int n) {
-        int N = m+n-2;
-        int r= m-1;
-        
-        double res=1;
-        
-        for(int i=1;i<=r;i++){
-            res=res*(N-r+i)/i;
+        int i=m-1,j=n-1;
+        int[][] dp=new int[m][n];
+        for (int[] row : dp){
+            Arrays.fill(row, -1);
         }
-        
-        return (int)res;
+        return f(i,j,dp);
     }
 }
