3c1da18c58ca2fc3e5be69524e9c8f0f490c1cc4
Pratik Raja <111995076+pratik43210@users.noreply.github.com>
2024-11-11 19:32:42 +0530

    Time: 264 ms (51.29%), Space: 42.2 MB (70.87%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,25 +1,17 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
         int n=a.length(),m=b.length();
-        //a should repeat atlest a min number of times so that length
-        //of a>=length of b
-        //we can get that by m/n but we might get rounded down value
-        //if m is not a exact multiple of n
-        //so we pweform ceiling division by adding n-1, -1 accounts
-        //for when m is exact multiple of n
+        
         int minRepeats=(m+n-1)/n;
         
-        // Create a string by repeating 'a' the minimum required number of times
-        String repeatedA = a.repeat(minRepeats);
+        String repeatedA=a.repeat(minRepeats);
         
-        // Check if b is a substring of repeatedA
-        if (repeatedA.contains(b)) return minRepeats;
-
-        // Repeat a one more time and check again
-        repeatedA += a;
-        if (repeatedA.contains(b)) return minRepeats + 1;
-
-        // If not found, return -1
+        if(repeatedA.contains(b)) return minRepeats;
+        
+        repeatedA+=a;
+        
+        if(repeatedA.contains(b)) return minRepeats+1;
+        
         return -1;
     }
 }
